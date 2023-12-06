58beae9c2b655a2fe7a2103a9e8ac494db748b6b
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-12-06 18:13:39 +0900

    231206_MyButton 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -6,21 +6,36 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+//COMPONENTS
+import MyButton from "./components/MyButton";
+
 function App() {
-  const env = process.env;
-  env.PUBLIC_URL = env.PUBLIC_URL || "";
+  // const env = process.env;
+  // env.PUBLIC_URL = env.PUBLIC_URL || "";
 
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
 
+        {/* 이미지 확인
         <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
         <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
         <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
         <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} /> */}
 
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
