93ad5a087bc8caead0ab2f1771cc8d58ba6ca899
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-12-11 21:47:24 +0900

    231211_CSS 추가

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -13,7 +13,7 @@ const DiaryEditor = () => {
 
   const navigate = useNavigate();
   return (
-    <div>
+    <div className="DiaryEditor">
       <MyHeader
         headText={"새 일기쓰기"}
         leftChild={
@@ -23,9 +23,9 @@ const DiaryEditor = () => {
       <div>
         <section>
           <h4>오늘은 언제인가요?</h4>
-          <div className="input-box">
+          <div className="input_box">
             <input
-              className="input-date"
+              className="input_date"
               value={date}
               onChange={(e) => setDate(e.target.value)}
               type="date"
