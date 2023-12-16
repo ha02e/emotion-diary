3e72ee77d88e1152037e949a52ceb9c7417d5a41
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-12-17 01:03:35 +0900

    231217_일기 상세페이지 구현 완료

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -6,40 +6,8 @@ import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
 import EmotionItem from "./EmotionItem";
 
-const env = process.env;
-env.PUBLIC_URL = env.PUBLIC_URL || "";
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "기분 최고!",
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: "기분 좋아",
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "우울해",
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: "화나",
-  },
-];
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
