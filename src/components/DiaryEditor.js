64c0a87682b1ad7fadb57fc349411b798a8ebf4b
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-12-20 17:04:18 +0900

    231220_일기 삭제 기능 추가 완료

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -19,7 +19,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
     setEmotion(emotion);
   };
 
-  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
 
   const navigate = useNavigate();
 
@@ -44,6 +44,13 @@ const DiaryEditor = ({ isEdit, originData }) => {
     navigate("/", { replace: true });
   };
 
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
   useEffect(() => {
     if (isEdit) {
       //수정하기 페이지
@@ -59,6 +66,15 @@ const DiaryEditor = ({ isEdit, originData }) => {
         headText={isEdit ? "일기 수정하기" : "새 일기쓰기"}
         leftChild={
           <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          isEdit && (
+            <MyButton
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={handleRemove}
+            />
+          )
         }
       />
       <div>
