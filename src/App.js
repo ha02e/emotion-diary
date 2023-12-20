824888c9ce57d8e87b0ecc7797efd95e63fbe74b
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-12-20 17:03:59 +0900

    231220_일기 데이터 localStorage에 저장

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef, useEffect } from "react";
+import React, { useReducer, useRef } from "react";
 
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -38,54 +38,16 @@ const reducer = (state, action) => {
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(newState));
   return newState;
 };
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: "오늘의 일기 1번",
-    date: 1702132478416,
-  },
-  {
-    id: 2,
-    emotion: 2,
-    content: "오늘의 일기 2번",
-    date: 1702132478417,
-  },
-  {
-    id: 3,
-    emotion: 3,
-    content: "오늘의 일기 3번",
-    date: 1702132478418,
-  },
-  {
-    id: 4,
-    emotion: 4,
-    content: "오늘의 일기 4번",
-    date: 1702132478419,
-  },
-  {
-    id: 5,
-    emotion: 5,
-    content: "오늘의 일기 5번",
-    date: 1702132478420,
-  },
-];
-
 function App() {
-  useEffect(() => {
-    const item1 = parseInt(localStorage.getItem("item1"));
-    const item2 = localStorage.getItem("item2");
-    const item3 = JSON.parse(localStorage.getItem("item3"));
-    console.log({ item1, item2, item3 });
-  }, []);
-
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  const [data, dispatch] = useReducer(reducer, []);
 
   const dataId = useRef(6);
 
