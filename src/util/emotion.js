37e5fb549c3f7be5ded57676053d5c86f71fb34a
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-12-23 00:28:34 +0900

    231223_감정 종류 추가

--- src/util/emotion.js
+++ src/util/emotion.js
@@ -5,7 +5,7 @@ export const emotionList = [
   {
     emotion_id: 1,
     emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "기분 최고!",
+    emotion_descript: "행복해",
   },
   {
     emotion_id: 2,
@@ -15,16 +15,31 @@ export const emotionList = [
   {
     emotion_id: 3,
     emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
+    emotion_descript: "설레어",
   },
   {
     emotion_id: 4,
     emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "우울해",
+    emotion_descript: "그럭저럭",
   },
   {
     emotion_id: 5,
     emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: "피곤해",
+  },
+  {
+    emotion_id: 6,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion6.png`,
+    emotion_descript: "우울해",
+  },
+  {
+    emotion_id: 7,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion7.png`,
     emotion_descript: "화나",
+  },
+  {
+    emotion_id: 8,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion8.png`,
+    emotion_descript: "아파",
   },
 ];
