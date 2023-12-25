02f53e353c30693740abcf79fb5e8a7ff8fbe99c
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-12-25 17:43:38 +0900

    231225_정렬 텍스트 수정

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -10,8 +10,8 @@ const sortOptionList = [
 
 const filterOptionList = [
   { value: "all", name: "전부다" },
-  { value: "good", name: "좋은 감정만" },
-  { value: "bad", name: "안좋은 감정만" },
+  { value: "good", name: "긍정적인" },
+  { value: "bad", name: "부정적인" },
 ];
 
 const ControlMenu = React.memo(({ value, onChange, optionList }) => {
